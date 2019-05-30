0e7f17afef9d2854994642de58d67d0d3fd151b2
Denilsonachola <31476457+Denilsonachola@users.noreply.github.com>
2019-05-30 12:42:52 +0300

    Add files via upload

---
+++ screencaster.php
@@ -0,0 +1,111 @@
+<?php
+?>
+
+<?php
+function numberTowords($num)
+{ 
+$ones = array( 
+1 => "one", 
+2 => "two", 
+3 => "three", 
+4 => "four", 
+5 => "five", 
+6 => "six", 
+7 => "seven", 
+8 => "eight", 
+9 => "nine", 
+10 => "ten", 
+11 => "eleven", 
+12 => "twelve", 
+13 => "thirteen", 
+14 => "fourteen", 
+15 => "fifteen", 
+16 => "sixteen", 
+17 => "seventeen", 
+18 => "eighteen", 
+19 => "nineteen" 
+); 
+$tens = array( 
+1 => "ten",
+2 => "twenty", 
+3 => "thirty", 
+4 => "forty", 
+5 => "fifty", 
+6 => "sixty", 
+7 => "seventy", 
+8 => "eighty", 
+9 => "ninety" 
+); 
+$hundreds = array( 
+"hundred", 
+"thousand", 
+"million", 
+"billion", 
+"trillion", 
+"quadrillion" 
+); //limit t quadrillion 
+$num = number_format($num,2,".",","); 
+$num_arr = explode(".",$num); 
+$wholenum = $num_arr[0]; 
+$decnum = $num_arr[1]; 
+$whole_arr = array_reverse(explode(",",$wholenum)); 
+krsort($whole_arr); 
+$rettxt = ""; 
+foreach($whole_arr as $key => $i){ 
+if($i < 20){ 
+$rettxt .= $ones[$i]; 
+}elseif($i < 100){ 
+$rettxt .= $tens[substr($i,0,1)]; 
+$rettxt .= " ".$ones[substr($i,1,1)]; 
+}else{ 
+$rettxt .= $ones[substr($i,0,1)]." ".$hundreds[0]; 
+$rettxt .= " ".$tens[substr($i,1,1)]; 
+$rettxt .= " ".$ones[substr($i,2,1)]; 
+} 
+if($key > 0){ 
+$rettxt .= " ".$hundreds[$key]." "; 
+} 
+} 
+if($decnum > 0){ 
+$rettxt .= " and "; 
+if($decnum < 20){ 
+$rettxt .= $ones[$decnum]; 
+}elseif($decnum < 100){ 
+$rettxt .= $tens[substr($decnum,0,1)]; 
+$rettxt .= " ".$ones[substr($decnum,1,1)]; 
+} 
+} 
+return $rettxt; 
+} 
+ 
+extract($_POST);
+if(isset($convert))
+{
+echo "<p align='center' style='color:blue'>".numberTowords("$num")."</p>";
+}
+ 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Convert your Number to words</title>
+</head>
+<body>
+ <form method="post">  
+Enter First Number:  
+<input type="number" name="number1" /><br><br>  
+Enter Second Number:  
+<input type="number" name="number2" /><br><br>  
+<input  type="submit" name="submit" value="Add">  
+</form>  
+<?php  
+    if(isset($_POST['submit']))  
+    {  
+        $number1 = $_POST['number1'];  
+        $number2 = $_POST['number2'];  
+        $sum =  $number1+$number2;     
+echo "The sum of $number1 and $number2 is: ".$sum;   
+}  
+?>  
+</body>
+</html>
